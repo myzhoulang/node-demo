266ab9795eefc612893aa09212a1a25dbe5536db
周浪 <lang.zhou@fulintechfin.com>
2018-09-13 18:35:07 +0800

    :bento: 添加查询单个用户

--- mock.js
+++ mock.js
@@ -9,18 +9,56 @@ app.get('/api/users', (req, res) => {
     'list|10': [{
       // 属性 id 是一个自增数，起始值为 1，每次增 1
       'id|+1': 1,
-      'name': function () {
+      'name': () => {
         return Mock.mock('@cname')
       },
+      'createTime': () => {
+        return Mock.mock('@datetime')
+      },
       'age|1-120': 1,
-      'address': function () {
+      'address': () => {
         return Mock.mock('@county(true)')
       }
     }]
-  })
-  res.json(data)
+  });
+  setTimeout(() => {
+    res.json(data);
+  }, 2000);
+});
+
+app.get('/api/users/:id', (req, res) => {
+  const params = req.params;
+  const data = Mock.mock({
+    // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+    'user': {
+      // 属性 id 是一个自增数，起始值为 1，每次增 1
+      'id': params.id,
+      'name': () => {
+        return Mock.mock('@cname')
+      },
+      'sex|0-1':'number',
+      'no|001': () => {
+        return Mock.mock('@integer(60, 100)')
+      },
+      'phone': () => {
+        return Mock.mock({
+          'regexp': /(^0{0,1}1[3|4|5|6|7|8|9][0-9]{9}$)/
+        })
+      },
+      'department|0-4': 'number',
+      'email': () => {
+        return Mock.mock('@email')
+      },
+      'roles': () => {
+        Mock.mock('@range(0, 4, 2)')
+      }
+    }
+  });
+  setTimeout(() => {
+    res.json(data);
+  }, 2000);
 })
 
 app.listen(3008, () => {
   console.log('api server is running');
-})
+});
